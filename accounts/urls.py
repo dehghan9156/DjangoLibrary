46ed304106209d960a9c6dd6d4d70cbfab07bb84
Dehghan <dehghan9156@gmail.com>
2025-03-31 16:16:40 +0330

    add register user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import views
+from . import views
 from django.urls import path,include
 
 app_name = 'accounts'
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from .forms import UserRegisterForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-
-class RegisterUserView(View):
-    pass
+class RegisterUserView(CreateView):
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('accounts:user-login')
+    form_class = UserRegisterForm
+    success_message = "User Create Successfully.Thank You"
